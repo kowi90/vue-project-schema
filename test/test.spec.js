89d4db6bf6f362020f552f59e0202b254a9ddde4
Gergő Kovács <gergo.kovacs@cheppers.com>
2018-03-25 23:57:11 +0200

    tests for all + fallback to shell ci

--- test/test.spec.js
+++ test/test.spec.js
@@ -12,21 +12,35 @@ test('Virtual filesystem is working', async (assert) => {
     assert.deepEqual(['/test.json'], dirStructure);
   })
 
-
-
-
-
 test('Correct files are generated on the right path', async (assert) => {
-    
+    // Defining virtual template paths
+    process.env.TEMPLATE_VUE_PATH = '/templates/template.vue';
+    process.env.TEMPLATE_SCSS_PATH = '/templates/template.scss';
+
+    vol.mkdirSync('/templates');
+    var vueTemplate = real.readFileSync('./lib/templates/template.vue', 'utf-8');
+    vol.writeFileSync(process.env.TEMPLATE_VUE_PATH, vueTemplate);
+    var scssTemplate = real.readFileSync('./lib/templates/template.scss', 'utf-8');
+    vol.writeFileSync(process.env.TEMPLATE_SCSS_PATH, scssTemplate);
     var obj = real.readFileSync('./test/test.json', 'utf-8');
     vol.writeFileSync('/test.json', obj);
-    
+
     var testModule = rewire("../lib/index.js");
 
     testModule.__set__("fs", vol);
-    testModule.convertJSON('/test.json', '.', false);
+    testModule.convertJSON('/test.json', '/', 'true');
     
     let dirStructure = Object.keys(vol.toJSON());
-    
-    assert.deepEqual(['/test.json'], dirStructure);
+    assert.deepEqual([
+    '/test.json',
+    '/templates/template.vue',
+    '/templates/template.scss',
+    '/testDir/newTestDir12/index.js',
+    '/testDir/newTestDir12/test.vue',
+    '/testDir/newTestDir23/new.vue',
+    '/testDir/testComponent/assets/styles/styles.scss',
+    '/testDir/testComponent/index.vue',
+    '/testDir/testComponent/components/nestedComponent/assets/styles/styles.scss',
+    '/testDir/testComponent/components/nestedComponent/index.vue'
+    ], dirStructure);
   })
